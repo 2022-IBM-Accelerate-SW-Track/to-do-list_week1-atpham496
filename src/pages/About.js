cd65c09125033c7de912ebcb20279f54c529da0b
atpham496 <anhthu.pham@sjsu.edu>
2022-06-11 00:06:13 -0700

    git commit -m 'About me description and picture'

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import abt_me_pic from "../assets/abt_me_pic.jpg";  
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        
+        <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src= {abt_me_pic}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Anh Thu Pham</div>
+        <div className="brief_description">
+          <p>Hi there, my name is Anh Thu Pham. I'm an undergraduate software engineering major from San Jose State University. I am also working on a Chinese minor as well.</p>
+
+          <p>My interests are in cybersecurity, programming, and using my creativity to build cool applications.</p> 
+
+          <p>Outside of coursework, my hobbies include listening to K-pop, gaming, and learning new foreign languages.</p>
+
+          <p>I can speak Vietnamese, English, and an intermediate level of Mandarin.</p>   
+        </div>
+      </div>
+    </div>
+  </div> 
       </div>
     )
   }
